4d994878718300063765b5972d9a3f70eac21cb0
Albin Chen <albin.chen@aktsk.com>
2022-10-04 21:13:35 +0800

    product_crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+
+Product.create!(title: 'I am robot',
+                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla facilis unde, libero laborum velit necessitatibus eos aut alias iure enim, dolores magnam cum iste fugiat nam laboriosam, temporibus tempora quibusdam.",
+                image_url: "https://robohash.org/123",
+                price: 20.00)
